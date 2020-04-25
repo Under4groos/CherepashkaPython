eb10916b73ab4d976b0af62ebfef1d40f8c45004
Under4groos <54286928+Under4groos@users.noreply.github.com>
2020-04-25 23:31:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
 
 
-
 from turtle import *
 import math
 Test = Turtle()
 Test.screen.setup(800, 800)
-
-
-def ang( id , count = 5 , Rad = 10 , Size_ = 10  ):
+def ang( id , count = 5 , Rad = 10 ):
     ang_ = 2 / count * id * 3.1415 # Радианах 
-
-    posX =  Size_ * Rad * math.sin( ang_ )
-    PosY =  Size_ * Rad * math.cos( ang_ )    
-
+    posX =  Rad * math.sin( ang_ )
+    PosY =  Rad * math.cos( ang_ )    
     return posX , PosY
-
 def rectangle(w, h):
-
-    count = 5
-    Size = 5 
-    Radius = 10 
-
+    count = 3
+    Radius = 66 
     for i in range( 0 , count + 1):
-
-        x , y = ang( i ,  count = count , Rad = Radius , Size_ = Size )
-        
+        x , y = ang( i ,  count = count , Rad = Radius )
         Test.goto( x , y )
+rectangle(320, 200)
+Test.screen.exitonclick()
+Test.screen.mainloop()
 
 rectangle(320, 200)
 Test.screen.exitonclick()
